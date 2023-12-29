c2e62a1076bca7b9414002c8adc6b64e5d644d88
lxf <929055273@qq.com>
2023-12-29 10:25:00 +0800

    2

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,11 @@
-#include "simulator/randomSl.h"
-#include "simulator/fifoSl.h"
+// #include "simulator/randomSl.h"
+// #include "simulator/fifoSl.h"
 #include "simulator/lruSl.h"
-#include "simulator/lfuSl.h"
-#include "simulator/lirsSl.h"
-#include "simulator/arcSl.h"
-#include "simulator/clockproSl.h"
-#include "simulator/2qSl.h"
+// #include "simulator/lfuSl.h"
+// #include "simulator/lirsSl.h"
+// #include "simulator/arcSl.h"
+// #include "simulator/clockproSl.h"
+// #include "simulator/2qSl.h"
 // #include "simulator/tinylfuSl.h"
 
 using namespace std;
@@ -13,30 +13,30 @@ using namespace std;
 int main(){
     Sl *sim = nullptr;
     switch(POLICY){
-        case PolicyType::RANDOM: 
-            sim = new RandomSl();
-            break;
-        case PolicyType::FIFO:
-            sim = new FifoSl();
-            break;
-        case PolicyType::LFU:
-            sim = new LfuSl();
-            break;
+        // case PolicyType::RANDOM: 
+        //     sim = new RandomSl();
+        //     break;
+        // case PolicyType::FIFO:
+        //     sim = new FifoSl();
+        //     break;
+        // case PolicyType::LFU:
+        //     sim = new LfuSl();
+        //     break;
         case PolicyType::LRU:
             sim = new LruSl();
             break;
-        case PolicyType::LIRS:
-            sim = new LirsSl();
-            break;
-        case PolicyType::ARC:
-            sim = new ArcSl();
-            break;
-        case PolicyType::CLOCKPRO:
-            sim = new ClockproSl();
-            break;
-        case PolicyType::TQ:
-            sim = new TqSl();
-            break;
+        // case PolicyType::LIRS:
+        //     sim = new LirsSl();
+        //     break;
+        // case PolicyType::ARC:
+        //     sim = new ArcSl();
+        //     break;
+        // case PolicyType::CLOCKPRO:
+        //     sim = new ClockproSl();
+        //     break;
+        // case PolicyType::TQ:
+        //     sim = new TqSl();
+        //     break;
         // case PolicyType::TINYLFU:
         //     sim = new TinylfuSl();
         //     break;
