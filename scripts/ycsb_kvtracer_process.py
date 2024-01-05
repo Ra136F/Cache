8d45dc4bf319533ed59c94062ed81d5d7f68519b
lxf <929055273@qq.com>
2024-01-05 18:28:46 +0800

    config

--- scripts/ycsb_kvtracer_process.py
+++ scripts/ycsb_kvtracer_process.py
@@ -79,10 +79,10 @@ def output_file(file_path, indexes, types, chunk_size, disk_size, trace_size):
 
 if __name__ == '__main__':
     # process trace
-    file_name = "tracea_runr.txt"
+    file_name = "tracea_run46.txt"
     input_path = "/home/lxf/work/code/Caching-Policy/Caching-Policy/trace/zipfian/"
     # output_path = "D:/Projects/Caching-Policy/Caching-Policy/trace/YCSB-KVTracer/"
-    output_path = "/home/lxf/work/code/Caching-Policy/Caching-Policy/trace/zipfian/100w_28_4k/"
+    output_path = "/home/lxf/work/code/Caching-Policy/Caching-Policy/trace/zipfian/100w_4k/"
     keys, types = read_trace_from_file(input_path + file_name)
     indexes, disk_size = frequency_counter(keys)
     chunk_size = 4 * 1024
