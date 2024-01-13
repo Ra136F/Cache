a44da8c1ed12cb8d49c64d6dfb0f01e44443769a
lxf <929055273@qq.com>
2024-01-13 19:58:38 +0800

    1

--- src/utils/config.h
+++ src/utils/config.h
@@ -7,14 +7,14 @@ const PolicyType POLICY = policyTypes[3]; // 0~8
 const long long DISK_SIZE = 100000; // disk_capacity=DISK_SIZE*CHUNK_SIZE
 const long long CHUNK_NUM = 100000;
 
-const char *TRACE_PATH = "../trace/zipfian/10w_82_4k/tracea_runr-1w.txt";
+const char *TRACE_PATH = "../trace/zipfian/10w_82_4k/tracea_runw-1w.txt";
 const char *CACHE_PATH = "/data2/Cache/cache_0.1.bin";
 const char *CACHE_PATH_W = "/data2/Cache/cache_0.2.bin";
 const long long CACHE_SIZE = CHUNK_NUM * 0.02;
 
 const long long HALF_SIZE = CACHE_SIZE*0.5;
 const long long P_SIZE = CACHE_SIZE*2;
-const char *DISK_PATH = "/SMR/db/storage/diskr-1w.bin";
+const char *DISK_PATH = "/SMR/db/storage/diskw-1w.bin";
 
 typedef long long ll;
 const long long CHUNK_SIZE = 4 * 1024; // 4kB
